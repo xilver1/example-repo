981142fd00a72e10d4fecf7642d9fefb8ac62df5
Giron <hiron@hiron.com>
2026-05-02 17:35:02 +0200

    Text change

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from EKS!")
+        self.wfile.write(b"Hello from EKS after an update to myself!")
 
 HTTPServer(("", 8080), Handler).serve_forever()
