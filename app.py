8effdc8efd46f8b54c5d6185b9e79aa518b2cfb1
Giron <hiron@hiron.com>
2026-05-02 19:36:42 +0200

    Some other change

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"I've FINALLY changed!")
+        self.wfile.write(b"I've WHAT? changed!")
 
 HTTPServer(("", 8080), Handler).serve_forever()
